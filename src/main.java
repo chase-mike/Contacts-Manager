5fa2484504e213e2a48f55f812a10b52b35001eb
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-02-27 10:34:02 -0600

    fixed curly brackets

--- src/main.java
+++ src/main.java
@@ -3,24 +3,19 @@ public class main {
 
         while(true){
             int userChoice = updateContacts.mainMenu();
-            if(userChoice == 1){
+            if(userChoice == 1) {
                 updateContacts.printList();
-            else if (userChoice == 2){
+            }else if (userChoice == 2) {
                 updateContacts.addPerson();
-            else if ( userChoice == 3){
+            }else if ( userChoice == 3) {
                 updateContacts.searchByLastName();
-            else if (userChoice == 4){
+            }else if (userChoice == 4) {
                 updateContacts.deleteContact
-            else if( userChoice == 5){
+            }else if( userChoice == 5){
                 updateContacts.exit();
            }else {
            System.out.println("Invalid Input");
-                            }
-                        }
-                    }
-                }
-            }
-        }
+
 
     }
 }
