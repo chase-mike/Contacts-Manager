665fae270ffc05ba29ee40ef57be009d63b499d0
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-02-27 10:18:29 -0600

    main updated

--- src/main.java
+++ src/main.java
@@ -1,6 +1,26 @@
 public class main {
     public static void main(String[] args) {
 
+        while(true){
+            int userChoice = updateContacts.mainMenu();
+            if(userChoice == 1){
+                updateContacts.printList();
+            else if (userChoice == 2){
+                updateContacts.addPerson();
+            else if ( userChoice == 3){
+                updateContacts.searchByLastName();
+            else if (userChoice == 4){
+                updateContacts.deleteContact
+            else if( userChoice == 5){
+                updateContacts.exit();
+           }else {
+           System.out.println("Invalid Input");
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
     }
 }
